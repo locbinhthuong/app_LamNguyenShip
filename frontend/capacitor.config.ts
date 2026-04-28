75124a3ee9fa43ac8ced707b1286266594f7650d
locbinhthuong <tanlocdepzai123@gmail.com>
2026-04-28 19:29:22 +0700

    Fix: convert capacitor config to json to avoid typescript requirement

--- frontend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'vn.lamnguyenship.customer',
-  appName: ' AloShipp',
-  webDir: 'dist'
-};
-
-export default config;
