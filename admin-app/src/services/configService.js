cd23d00380670fff1d426d17988019920f9ce2cf
locbinhthuong <tanlocdepzai123@gmail.com>
2026-05-24 00:16:36 +0700

    fix: Correct config API URL

--- admin-app/src/services/configService.js
+++ admin-app/src/services/configService.js
@@ -1,11 +1,11 @@
 import api from './api';
 
 export const getPricingConfig = async () => {
-  const response = await api.get('/config/PRICING_CONFIG');
+  const response = await api.get('/api/config/PRICING_CONFIG');
   return response.data;
 };
 
 export const updatePricingConfig = async (configData) => {
-  const response = await api.put('/config/PRICING_CONFIG', { value: configData });
+  const response = await api.put('/api/config/PRICING_CONFIG', { value: configData });
   return response.data;
 };
