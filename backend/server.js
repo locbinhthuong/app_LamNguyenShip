bfecfff38d6a28ddabaa251543ab3cdd074640d4
locbinhthuong <tanlocdepzai123@gmail.com>
2026-07-09 00:07:33 +0700

    chore: support vercel serverless for backend demo

--- backend/server.js
+++ backend/server.js
@@ -260,3 +260,6 @@ const gracefulShutdown = (signal) => {
 
 process.on('SIGTERM', () => gracefulShutdown('SIGTERM'));
 process.on('SIGINT', () => gracefulShutdown('SIGINT'));
+
+module.exports = app;
+
